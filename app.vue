703a98ca08b4ac0ef8c6099c7f4bca685d465335
emalisuranga <37981046+emalisuranga@users.noreply.github.com>
2024-03-06 14:27:41 +0900

    add vuetify

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <NuxtLayout>
+      <v-app>
         <NuxtPage />
+      </v-app>
     </NuxtLayout>
   </div>
 </template>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,28 +1,46 @@
 <script setup>
 import { signOut } from "aws-amplify/auth";
-import { useRouter } from 'vue-router'
+import { getCurrentUser } from "aws-amplify/auth";
 const router = useRouter()
 
 const handleSignOut = async () => {
   try {
     await signOut({ global: true });
-    router.push('/login')
   } catch (error) {
     console.log("error signing out: ", error);
   }
 };
+
+onMounted(async () => {
+  try {
+    // Check if the user is authenticated
+    const currentUser = await getCurrentUser()
+    console.log(currentUser);
+    if (currentUser) {
+      localStorage.setItem('accessToken', currentUser.userId)
+    } else {
+    //   router.push('/login')
+    }
+  } catch (error) {
+    console.error('Error getting current user:', error)
+  }
+})
 </script>
 
-<template>
-  <!-- <authenticator>
-    <template v-slot="{ user, signOut }">
-      <h1>Hello {{ user.username }}!</h1>
-      <button @click="signOut">Sign Out</button>
-    </template>
-  </authenticator> -->
+<!-- <template>
   <h1>dashboard</h1>
   <button
   @click="handleSignOut"
             class="inline-flex w-full items-center justify-center rounded-md border border-gray-300 bg-white dark:bg-gray-700 px-4 py-2 text-sm font-medium text-gray-500 dark:text-white shadow-sm hover:bg-gray-50 dark:hover:bg-gray-600 disabled:cursor-wait disabled:opacity-50"
           >Sing Out</button>
-</template>
+</template> -->
+
+<template>
+    <v-layout>
+      <!-- <side-nav /> -->
+      <application-bar />
+  
+      <v-main style="height: 250px">
+      </v-main>
+    </v-layout>
+  </template>
