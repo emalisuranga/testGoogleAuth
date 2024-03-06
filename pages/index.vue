c31810c34a45d203ffbb68738001f6e636da77dc
emalisuranga <37981046+emalisuranga@users.noreply.github.com>
2024-03-06 16:46:14 +0900

    change login

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,5 @@
-<script setup>
-import { Authenticator } from "@aws-amplify/ui-vue";
-import "@aws-amplify/ui-vue/styles.css";
-</script>
-
-<template>
-  <authenticator>
-    <template v-slot="{ user, signOut }">
-      <h1>Hello {{ user.username }}!</h1>
-      <button @click="signOut">Sign Out</button>
-    </template>
-  </authenticator>
-</template>
-<!-- 
 <template>
     <login>
       <google-auth-btn />
     </login>
-</template> -->
+</template>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,5 +1,13 @@
+<script setup>
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+</script>
+
 <template>
-    <login>
-      <google-auth-btn />
-    </login>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
 </template>
