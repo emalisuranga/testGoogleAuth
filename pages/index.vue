9b13536dcb39d5a59d830608bef08c3b759486dc
emalisuranga <37981046+emalisuranga@users.noreply.github.com>
2024-03-06 15:59:22 +0900

    change ui

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,17 @@ import { Authenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
 </script>
 
-<!-- <template>
+<template>
   <authenticator>
     <template v-slot="{ user, signOut }">
       <h1>Hello {{ user.username }}!</h1>
       <button @click="signOut">Sign Out</button>
     </template>
   </authenticator>
-</template> -->
+</template>
 
-<template>
+<!-- <template>
     <login>
       <google-auth-btn />
     </login>
-</template>
+</template> -->
